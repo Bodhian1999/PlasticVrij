659096a127fe0d696edb36b74f726440130f76e7
Lauren Meenhorst <53778292+LaurenWritesCode@users.noreply.github.com>
2023-06-14 17:53:36 +0200

    Update form.py

--- form.py
+++ form.py
@@ -9,7 +9,11 @@ def form_page(current_user_email):
     st.write(f"Huidige gebruiker: {current_user_email}")
 
     # Form inputs
+<<<<<<< Updated upstream
     inputs = [
+=======
+    categories = [
+>>>>>>> Stashed changes
         ("rietjes", "Gebruik je 'rietjes'?"),
         ("honingstaafjes", "Gebruik je 'honingstaafjes'?"),
         ("melkcupjes", "Gebruik je 'melkcupjes'?"),
@@ -30,29 +34,33 @@ def form_page(current_user_email):
         ("ijsjes_plastic_verpakking", "Gebruik je 'ijsjes (met plastic verpakking)'?"),
         ("natte_doekjes_garnalen_spareribs", "Gebruik je 'natte doekjes (na garnalen/spare-ribs)'?")
     ]
-
+    
     responses = {"Email": current_user_email}  # Add current user's email to responses
 
-    for i, (category, question) in enumerate(inputs):
+    for i, (category, question) in enumerate(categories):
         uses_category = st.selectbox(question, ("Nee", "Ja"), key=f"{category}_selectbox_{i}")
         responses[category] = uses_category
-        product_category = st.selectbox("Onder welke categorie valt dit product?", ("Multi-Use Non-Plastics", "Single-Use Non-Plastics", "n.v.t. (product uit assortiment gehaald)", "Single-Use Plastics (alleen kiezen wanneer 'Ja' is geantwoord)"), key=f"{category}_alternative_{i}")
-        responses[f"product_category_{category}"] = product_category
 
-        if product_category != "n.v.t. (product uit assortiment gehaald)":
-            aantal_category = st.number_input(f"Hoeveel {category}s gebruik je per jaar?", min_value=0, step=1)
+        if uses_category == "Ja":
+            aantal_category = st.number_input(f"Hoeveel {category} gebruik je per jaar?", min_value=0, step=1)
             prijs_per_category = st.number_input(f"Wat is de prijs per {category}?", min_value=0.0, step=0.01)
-            responses[f"aantal_{category}s"] = aantal_category
+            responses[f"aantal_{category}"] = aantal_category
             responses[f"prijs_per_{category}"] = prijs_per_category
-        else:
-            responses[f"aantal_{category}s"] = np.nan
-            responses[f"prijs_per_{category}"] = np.nan
+            
+        if uses_category == "Nee":
+            chosen_alternative = st.selectbox("Voor welk alternatief heb je gekozen?", ("Weglaten", "Herbruikbaar", "Afbreekbaar"), key=f"{category}_alternative_{i}")
+            responses[f"chosen_alternative_{category}"] = chosen_alternative
+            if chosen_alternative != "Weglaten":
+                aantal_category = st.number_input(f"Hoeveel {category} gebruik je per jaar?", min_value=0, step=1)
+                prijs_per_category = st.number_input(f"Wat is de prijs per {category}?", min_value=0.0, step=0.01)
+                responses[f"aantal_{category}"] = aantal_category
+                responses[f"prijs_per_{category}"] = prijs_per_category
 
-        if (i + 1) % 3 == 0 or i == len(inputs) - 1:
+        if (i + 1) % 3 == 0 or i == len(categories) - 1:
             st.markdown('</div>', unsafe_allow_html=True)
 
     submit_button = st.button("Verzenden")
-
+    
     # Form submission handling
     if submit_button:
         utils.insert_form_responses(responses)
