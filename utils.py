ff015fc4fc99514cececf2337bea974f4938e573
Lauren Meenhorst <53778292+LaurenWritesCode@users.noreply.github.com>
2023-06-22 12:14:36 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -139,11 +139,10 @@ def get_recent_form_response(current_user_email):
     conn.close()
 
     if response:
-       # Convert the responses to a dataframe
+        # Convert the response tuple to a dictionary
         columns = [column[0] for column in cursor.description]
-        response_data = [dict(zip(columns, response)) for response in responses]
-        response_df = pd.DataFrame(response_data)
-        return response_df
+        response_dict = dict(zip(columns, response))
+        return response_dict
     else:
         return None
     
