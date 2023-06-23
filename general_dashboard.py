9d5580587d12d8fadfa2b4d84a5a7cb811ef308d
Lauren Meenhorst <53778292+LaurenWritesCode@users.noreply.github.com>
2023-06-23 15:35:53 +0200

    Update general_dashboard.py

--- general_dashboard.py
+++ general_dashboard.py
@@ -59,100 +59,102 @@ def general_dashboard_page(current_user_email):
         )
         st.subheader("Duurzaamheidspercentage over tijd")
         st.plotly_chart(fig)
+    
+
+        # Dataframe moet column met 'score', 'name' en 'postalcode' bevatten
+        user_df = get_all_user_data()
+
+        df = pd.DataFrame()
+        df['postal_code'] = user_df['postal_code']
+        df['name'] = user_df['company_name']
+        df['score'] = (recent_form_responses_df['Sustainability Percentage']/10)
+        df = df.iloc[1:]
+        # st.dataframe(df)
+
+        # df = pd.DataFrame({'postal_code': ['1102 TS', '1057 AS', '1016 AA', '1016AD']})
+        # df['score'] = [9,6,3,7]
+        # df['name'] = ['Pannenkoekenhuis','Thuis','UvA','Test']
+
+        def address_to_coordinates(postal_code):
+            address = f"{postal_code}, Netherlands"
+            geolocator = Nominatim(user_agent="my_geocoder")
+
+            try:
+                location = geolocator.geocode(address, exactly_one=True, country_codes="NL", timeout=10)
+            except GeocoderTimedOut:
+                return address_to_coordinates(postal_code)  # Retry on timeout
+
+            if location is None:
+                return None
+
+            latitude = location.latitude
+            longitude = location.longitude
+            return latitude, longitude
+
+        # Create an empty list
+        data_list = []
+
+        # Process each data point and add to the list
+        for _, row in df.iterrows():
+            postal_code = row['postal_code']
+            coordinates = address_to_coordinates(postal_code)
+
+            if coordinates is not None:
+                latitude, longitude = coordinates
+                data_list.append({'postal_code': postal_code, 'latitude': latitude, 'longitude': longitude})
+            else:
+                data_list.append({'postal_code': postal_code, 'latitude': None, 'longitude': None})
+
+        # Create a DataFrame from the list
+        coordinates_df = pd.DataFrame(data_list)
+            # st.dataframe(coordinates_df)
+        # Merge the original DataFrame with the coordinates DataFrame
+        df = pd.merge(df, coordinates_df, on='postal_code', how='left')
+
+        # Sample data with scores and coordinates
+        data = df
+
+        # Define a function to determine the color based on the score
+        def get_color(score):
+            if score >= (2/3)*10:
+                return 'green'  
+            elif score >= (1/3)*10:
+                return 'orange'  
+            else:
+                return 'red'  
+
+        # Create a Folium map centered on Amsterdam
+        m = folium.Map(location=[52.359425, 4.903517], zoom_start=11.5, width=800, height=600)
+
+        # Add markers to the map
+        for index, row in data.iterrows():
+            name = row['name']
+            score = row['score']
+            latitude = row['latitude']
+            longitude = row['longitude']
+            color = get_color(score)
+
+            # Create the Folium icon with the desired color and icon prefix
+            icon = folium.Icon(color=color, icon='map-pin', prefix='fa')
+
+            # Define the style for the score text
+            score_style = f'color: {color}; font-weight: bold; white-space: nowrap;'
+
+            # Create the Folium marker and add it to the map
+            folium.Marker(
+                location=[latitude, longitude],
+                popup=f'<b>{name}</b><br><span style="{score_style}">Score: {score}</span>',
+                icon=icon
+            ).add_to(m)
+
+        # Display the map
+        st.markdown('**Kaart**')
+        folium_static(m)
+
+        # Example: Display general data
+        st.write("General data goes here")
+    
     else:
         st.write("Er zijn geen ingevulde formulieren gevonden")
-
-    # Dataframe moet column met 'score', 'name' en 'postalcode' bevatten
-    user_df = get_all_user_data()
-
-    df = pd.DataFrame()
-    df['postal_code'] = user_df['postal_code']
-    df['name'] = user_df['company_name']
-    df['score'] = (recent_form_responses_df['Sustainability Percentage']/10)
-    df = df.iloc[1:]
-    # st.dataframe(df)
-    
-    # df = pd.DataFrame({'postal_code': ['1102 TS', '1057 AS', '1016 AA', '1016AD']})
-    # df['score'] = [9,6,3,7]
-    # df['name'] = ['Pannenkoekenhuis','Thuis','UvA','Test']
-
-    def address_to_coordinates(postal_code):
-        address = f"{postal_code}, Netherlands"
-        geolocator = Nominatim(user_agent="my_geocoder")
-
-        try:
-            location = geolocator.geocode(address, exactly_one=True, country_codes="NL", timeout=10)
-        except GeocoderTimedOut:
-            return address_to_coordinates(postal_code)  # Retry on timeout
-
-        if location is None:
-            return None
-
-        latitude = location.latitude
-        longitude = location.longitude
-        return latitude, longitude
-
-    # Create an empty list
-    data_list = []
-
-    # Process each data point and add to the list
-    for _, row in df.iterrows():
-        postal_code = row['postal_code']
-        coordinates = address_to_coordinates(postal_code)
-
-        if coordinates is not None:
-            latitude, longitude = coordinates
-            data_list.append({'postal_code': postal_code, 'latitude': latitude, 'longitude': longitude})
-        else:
-            data_list.append({'postal_code': postal_code, 'latitude': None, 'longitude': None})
-
-    # Create a DataFrame from the list
-    coordinates_df = pd.DataFrame(data_list)
-        # st.dataframe(coordinates_df)
-    # Merge the original DataFrame with the coordinates DataFrame
-    df = pd.merge(df, coordinates_df, on='postal_code', how='left')
-
-    # Sample data with scores and coordinates
-    data = df
-    
-    # Define a function to determine the color based on the score
-    def get_color(score):
-        if score >= (2/3)*10:
-            return 'green'  
-        elif score >= (1/3)*10:
-            return 'orange'  
-        else:
-            return 'red'  
-
-    # Create a Folium map centered on Amsterdam
-    m = folium.Map(location=[52.359425, 4.903517], zoom_start=11.5, width=800, height=600)
-
-    # Add markers to the map
-    for index, row in data.iterrows():
-        name = row['name']
-        score = row['score']
-        latitude = row['latitude']
-        longitude = row['longitude']
-        color = get_color(score)
-    
-        # Create the Folium icon with the desired color and icon prefix
-        icon = folium.Icon(color=color, icon='map-pin', prefix='fa')
-    
-        # Define the style for the score text
-        score_style = f'color: {color}; font-weight: bold; white-space: nowrap;'
-    
-        # Create the Folium marker and add it to the map
-        folium.Marker(
-            location=[latitude, longitude],
-            popup=f'<b>{name}</b><br><span style="{score_style}">Score: {score}</span>',
-            icon=icon
-        ).add_to(m)
-
-    # Display the map
-    st.markdown('**Kaart**')
-    folium_static(m)
-    
-    # Example: Display general data
-    st.write("General data goes here")
     
 
