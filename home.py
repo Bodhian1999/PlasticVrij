1df17a13a490163d919edeac060a2cd36f2dafb6
Lauren Meenhorst <53778292+LaurenWritesCode@users.noreply.github.com>
2023-06-26 12:27:30 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,13 +1,32 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import os
-import utils
-from decimal import Decimal
-from utils import get_recent_form_response
 
 def home_page(current_user_email):
     st.header("Welkom!")
 
     # Display the active user's email
     st.write(f"Huidige gebruiker: {current_user_email}")
+
+    # Add an inviting image or logo
+    st.image("path_to_image/logo.png", use_column_width=True)
+
+    # Introduction
+    st.write("Welkom bij ons plasticvrije terras-initiatief! Hier kun je jouw bijdrage aan het verminderen van plastic op terrassen volgen en delen. Ontdek je duurzaamheidsscore, vergelijk deze met andere ondernemers en zie de voortgang die we samen boeken!")
+
+    # Add an image related to plastic-free terraces
+    st.image("path_to_image/plastic_free_terraces.jpg", use_column_width=True)
+
+    # Explanation of the app
+    st.write("Wat kun je doen op deze app?")
+    st.write("- Vul het formulier in om jouw gebruik van plastic op het terras te registreren.")
+    st.write("- Volg jouw duurzaamheidsscore en zie hoe deze zich verhoudt tot andere ondernemers.")
+    st.write("- Ontdek tips en advies om plasticverbruik te verminderen en duurzame alternatieven te implementeren.")
+    st.write("- Bekijk de interactieve kaart en zie waar andere ondernemers zich bevinden.")
+    st.write("- Blijf op de hoogte van de laatste ontwikkelingen op het gebied van plasticvrije terrassen.")
+
+    # Add more visuals or images to illustrate the features and benefits of the app
+
+    # Call-to-action
+    st.write("Doe mee en maak jouw terras plasticvrij!")
+
+    # Add a visually appealing image or illustration related to plastic-free terraces
+    st.image("path_to_image/plastic_free_terrace_illustration.jpg", use_column_width=True)
