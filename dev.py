bb14dc200f95214b6e61bf17066315a93b7be03c
Lauren Meenhorst <53778292+LaurenWritesCode@users.noreply.github.com>
2023-06-27 15:32:22 +0200

    Update dev.py

--- dev.py
+++ dev.py
@@ -2,80 +2,57 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+from utils import get_recent_form_response, get_all_form_responses, calculate_sustainability_percentage, get_recent_form_responses, calculate_avg_sustainability_percentage, calculate_sustainability_score
 
 def dev(current_user_email):
     st.header("Overzicht van jouw Duurzaamheidsinspanningen")
     st.write(f"Huidige gebruiker: {current_user_email}")
 
-    categories = [
-        'rietjes', 'honingstaafjes', 'melkcupjes', 'suikerzakjes', 'koekjeswrappers',
-        'theezakjes_verpakking', 'ontbijt_boter', 'ontbijt_jam_pindakaas_chocoladepasta',
-        'saus_mayonaise', 'saus_ketchup', 'saus_mosterd', 'saus_soya_saus',
-        'pepermuntverpakking', 'snoepjes_rekening', 'tandenstokerverpakking',
-        'stampers', 'wegwerpbekers_feesten_partijen', 'ijsjes_plastic_verpakking',
-        'natte_doekjes_garnalen_spareribs'
-    ]
+    form_responses_df = get_all_form_responses(current_user_email)
 
-    selected_category = st.selectbox("Select Category", categories)
+    recent_form_responses_df = get_recent_form_responses()
 
-    # Average costs per year for each category
-    multi_use_non_plastics_cost = [100, 50, 70, 60, 80, 70, 80, 90, 100, 90, 70, 80, 60, 100, 110, 70, 80, 90, 70]
-    single_use_non_plastics_cost = [60, 40, 50, 40, 30, 40, 50, 50, 60, 50, 40, 50, 30, 70, 80, 40, 50, 60, 40]
-    multi_use_plastics_cost = [60, 30, 40, 50, 30, 40, 50, 60, 70, 60, 50, 40, 30, 80, 90, 50, 40, 50, 30]
-    single_use_plastics_cost = [20, 10, 15, 20, 10, 15, 20, 20, 30, 20, 15, 10, 10, 40, 50, 10, 15, 20, 10]
+    if form_responses_df is not None:
+        st.write("---")
+        st.subheader("Ingezonden formulierreacties")
+        st.write("Hieronder vindt je de door jou ingezonden formulierreacties.")
 
-    # Replacement intervals for each category
-    multi_use_non_plastics_replace_interval = 5
-    single_use_non_plastics_replace_interval = 1
-    multi_use_plastics_replace_interval = 3
-    single_use_plastics_replace_interval = 1
+        # Generate sample data for cost savings comparison
+        categories = [
+            'rietjes', 'honingstaafjes', 'melkcupjes', 'suikerzakjes', 'koekjeswrappers',
+            'theezakjes_verpakking', 'ontbijt_boter', 'ontbijt_jam_pindakaas_chocoladepasta',
+            'saus_mayonaise', 'saus_ketchup', 'saus_mosterd', 'saus_soya_saus',
+            'pepermuntverpakking', 'snoepjes_rekening', 'tandenstokerverpakking',
+            'stampers', 'wegwerpbekers_feesten_partijen', 'ijsjes_plastic_verpakking',
+            'natte_doekjes_garnalen_spareribs'
+        ]
 
-    # Generate sample data for cost comparison
-    years = np.arange(1, 11)  # Years 1 to 10
+        selected_category = st.selectbox("Select Category", categories)
 
-    # Calculate cost per year for each category
-    single_use_plastic_cost = single_use_plastics_cost[categories.index(selected_category)]
-    multi_use_non_plastic_cost = multi_use_non_plastics_cost[categories.index(selected_category)]
-    multi_use_plastic_cost = multi_use_plastics_cost[categories.index(selected_category)]
-    single_use_non_plastics_cost = single_use_non_plastics_cost[categories.index(selected_category)]
+        # Generate sample data for cost comparison
+        years = np.arange(1, 11)  # Years 1 to 10
+        single_use_plastic_cost = np.full(len(years), 1000)  # Cost of single-use plastics per year
+        multi_use_non_plastic_investment = 3000  # Initial investment for multi-use non-plastics
+        multi_use_non_plastic_cost = np.full(len(years), multi_use_non_plastic_investment)  # Cost of multi-use non-plastics
 
-    single_use_plastic_cost_per_year = [
-        single_use_plastics_cost if year % single_use_plastics_replace_interval == 0 else 0
-        for year in years
-    ]
+        # Increase the cost of multi-use non-plastics every 5 years
+        replace_interval = 5  # Number of years before replacement
+        replacement_cost = 3000  # Cost of replacement
+        for i in range(replace_interval, len(years), replace_interval):
+            multi_use_non_plastic_cost[i:] += replacement_cost
 
+        # Create the cost savings comparison line plot
+        data = pd.DataFrame({
+            'Years': years,
+            'Single-Use Plastics': np.cumsum(single_use_plastic_cost),
+            'Multi-Use Non-Plastics': multi_use_non_plastic_cost
+        })
 
-    multi_use_non_plastic_cost_per_year = [
-        multi_use_non_plastics_cost * np.ceil(year / multi_use_non_plastics_replace_interval)
-        if year % multi_use_non_plastics_replace_interval == 0
-        else multi_use_non_plastics_cost
-        for year in years
-    ]
+        selected_data = data[['Years', 'Single-Use Plastics', 'Multi-Use Non-Plastics']]
 
-    multi_use_plastic_cost_per_year = [
-        multi_use_plastics_cost * np.ceil(year / multi_use_plastics_replace_interval)
-        if year % multi_use_plastics_replace_interval == 0
-        else multi_use_plastics_cost
-        for year in years
-    ]
+        fig = px.line(selected_data, x='Years', y=['Single-Use Plastics', 'Multi-Use Non-Plastics'], title='Cost Savings Comparison: Plastic vs. Sustainable Alternatives')
+        fig.update_layout(xaxis_title='Years', yaxis_title='Cost (€)')
+        st.plotly_chart(fig)  # Display the plot using Streamlit's `st.plotly_chart` function
 
-    single_use_non_plastics_cost_per_year = np.repeat(single_use_non_plastics_cost, len(years))
-
-    # Create the cost savings comparison line plot
-    data = pd.DataFrame({
-        'Years': years,
-        'Single-Use Plastics': single_use_plastic_cost_per_year,
-        'Multi-Use Non-Plastics': multi_use_non_plastic_cost_per_year,
-        'Multi-Use Plastics': multi_use_plastic_cost_per_year,
-        'Single-Use Non-Plastics': single_use_non_plastics_cost_per_year
-    })
-
-    selected_data = data[
-        ['Years', 'Single-Use Plastics', 'Multi-Use Non-Plastics', 'Multi-Use Plastics', 'Single-Use Non-Plastics']
-    ]
-
-    fig = px.line(selected_data, x='Years',
-                  y=['Single-Use Plastics', 'Multi-Use Non-Plastics', 'Multi-Use Plastics', 'Single-Use Non-Plastics'],
-                  title='Cost Savings Comparison: Plastic vs. Sustainable Alternatives')
-    fig.update_layout(xaxis_title='Years', yaxis_title='Cost ($)')
-    st.plotly_chart(fig)  # Display the plot using Streamlit's `st.plotly_chart` function
+    else:
+        st.write("Er zijn nog geen formulierreacties gevonden voor de huidige gebruiker.")
