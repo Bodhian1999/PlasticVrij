dce3c715cc27ab479dfc159eda7e582d8966f40e
Lauren Meenhorst <53778292+LaurenWritesCode@users.noreply.github.com>
2023-06-27 13:50:44 +0200

    Update dev.py

--- dev.py
+++ dev.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-from utils import get_recent_form_response, get_all_form_responses, calculate_sustainability_percentage, get_recent_form_responses, calculate_avg_sustainability_percentage, calculate_sustainability_score
 
 def dev(current_user_email):
     st.header("Overzicht van jouw Duurzaamheidsinspanningen")
@@ -16,18 +15,6 @@ def dev(current_user_email):
         st.write("---")
         st.subheader("Ingezonden formulierreacties")
         st.write("Hieronder vindt je de door jou ingezonden formulierreacties.")
-
-        # Generate sample data for cost savings comparison
-        categories = [
-            'rietjes', 'honingstaafjes', 'melkcupjes', 'suikerzakjes', 'koekjeswrappers',
-            'theezakjes_verpakking', 'ontbijt_boter', 'ontbijt_jam_pindakaas_chocoladepasta',
-            'saus_mayonaise', 'saus_ketchup', 'saus_mosterd', 'saus_soya_saus',
-            'pepermuntverpakking', 'snoepjes_rekening', 'tandenstokerverpakking',
-            'stampers', 'wegwerpbekers_feesten_partijen', 'ijsjes_plastic_verpakking',
-            'natte_doekjes_garnalen_spareribs'
-        ]
-
-        selected_category = st.selectbox("Select Category", categories)
 
         # Generate sample data for cost comparison
         years = np.arange(1, 11)  # Years 1 to 10
