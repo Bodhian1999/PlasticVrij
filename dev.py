4d7189cb4565be0fa52335a2b56df8824eef079b
Lauren Meenhorst <53778292+LaurenWritesCode@users.noreply.github.com>
2023-06-27 15:40:17 +0200

    Update dev.py

--- dev.py
+++ dev.py
@@ -18,40 +18,50 @@ def dev(current_user_email):
         st.write("Hieronder vindt je de door jou ingezonden formulierreacties.")
 
         # Generate sample data for cost savings comparison
-        categories = [
-            'rietjes', 'honingstaafjes', 'melkcupjes', 'suikerzakjes', 'koekjeswrappers',
-            'theezakjes_verpakking', 'ontbijt_boter', 'ontbijt_jam_pindakaas_chocoladepasta',
-            'saus_mayonaise', 'saus_ketchup', 'saus_mosterd', 'saus_soya_saus',
-            'pepermuntverpakking', 'snoepjes_rekening', 'tandenstokerverpakking',
-            'stampers', 'wegwerpbekers_feesten_partijen', 'ijsjes_plastic_verpakking',
-            'natte_doekjes_garnalen_spareribs'
-        ]
-
-        selected_category = st.selectbox("Select Category", categories)
-
-        # Generate sample data for cost comparison
         years = np.arange(1, 11)  # Years 1 to 10
-        single_use_plastic_cost = np.full(len(years), 1000)  # Cost of single-use plastics per year
-        multi_use_non_plastic_investment = 3000  # Initial investment for multi-use non-plastics
-        multi_use_non_plastic_cost = np.full(len(years), multi_use_non_plastic_investment)  # Cost of multi-use non-plastics
 
-        # Increase the cost of multi-use non-plastics every 5 years
-        replace_interval = 5  # Number of years before replacement
-        replacement_cost = 3000  # Cost of replacement
-        for i in range(replace_interval, len(years), replace_interval):
-            multi_use_non_plastic_cost[i:] += replacement_cost
+        # Calculate the cost of reusable straws (steel)
+        reusable_steel_straws_cost = np.full(len(years), 100)  # Cost of 200 reusable straws (steel)
+        replace_interval_steel = 5  # Number of years before replacement
+        replacement_cost_steel = 100  # Cost of replacement
+        for i in range(replace_interval_steel, len(years), replace_interval_steel):
+            reusable_steel_straws_cost[i:] += replacement_cost_steel
+
+        # Calculate the cost of reusable straws (silicone)
+        reusable_silicone_straws_cost = np.full(len(years), 60)  # Cost of 200 reusable straws (silicone)
+        replace_interval_silicone = 2  # Number of years before replacement
+        replacement_cost_silicone = 60  # Cost of replacement
+        for i in range(replace_interval_silicone, len(years), replace_interval_silicone):
+            reusable_silicone_straws_cost[i:] += replacement_cost_silicone
+
+        # Calculate the cost of paper straws (single-use non-plastic)
+        paper_straws_cost = np.full(len(years), 60)  # Cost of 1000 paper straws
+        replace_interval_paper = 1  # Number of years before replacement
+        replacement_cost_paper = 60  # Cost of replacement
+        for i in range(replace_interval_paper, len(years), replace_interval_paper):
+            paper_straws_cost[i:] += replacement_cost_paper
+
+        # Calculate the cost of plastic straws (single-use plastic)
+        plastic_straws_cost = np.full(len(years), 20)  # Cost of 1000 plastic straws
+        replace_interval_plastic = 1  # Number of years before replacement
+        replacement_cost_plastic = 20  # Cost of replacement
+        for i in range(replace_interval_plastic, len(years), replace_interval_plastic):
+            plastic_straws_cost[i:] += replacement_cost_plastic
 
         # Create the cost savings comparison line plot
         data = pd.DataFrame({
             'Years': years,
-            'Single-Use Plastics': np.cumsum(single_use_plastic_cost),
-            'Multi-Use Non-Plastics': multi_use_non_plastic_cost
+            'Reusable Steel Straws': reusable_steel_straws_cost,
+            'Reusable Silicone Straws': reusable_silicone_straws_cost,
+            'Paper Straws': paper_straws_cost,
+            'Plastic Straws': plastic_straws_cost
         })
 
-        selected_data = data[['Years', 'Single-Use Plastics', 'Multi-Use Non-Plastics']]
+        selected_data = data[['Years', 'Reusable Steel Straws', 'Reusable Silicone Straws', 'Paper Straws', 'Plastic Straws']]
 
-        fig = px.line(selected_data, x='Years', y=['Single-Use Plastics', 'Multi-Use Non-Plastics'], title='Cost Savings Comparison: Plastic vs. Sustainable Alternatives')
-        fig.update_layout(xaxis_title='Years', yaxis_title='Cost (€)')
+        fig = px.line(selected_data, x='Years', y=['Reusable Steel Straws', 'Reusable Silicone Straws', 'Paper Straws', 'Plastic Straws'], 
+                      title='Cost Savings Comparison: Straws - Plastic vs. Sustainable Alternatives')
+        fig.update_layout(xaxis_title='Years', yaxis_title='Cost ($)')
         st.plotly_chart(fig)  # Display the plot using Streamlit's `st.plotly_chart` function
 
     else:
